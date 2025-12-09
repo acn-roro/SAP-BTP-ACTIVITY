773a3d24f761f107631803ee0c5790e2db7ed368
Ronan Chavarria <ronan.chavarria@accenture.com>
2025-12-09 06:15:26 +0000

    frontend

---
+++ app/febootcamp/webapp/controller/Home.controller.ts
@@ -0,0 +1,16 @@
+import Controller from "sap/ui/core/mvc/Controller";
+import MessageBox from "sap/m/MessageBox";
+
+/**
+ * @namespace febootcamp.controller
+ */
+export default class Home extends Controller {
+
+    /*eslint-disable @typescript-eslint/no-empty-function*/
+    
+public onButtonPress(): void {
+        MessageBox.show("This button is working, yey!", {
+            title: "Success" // Add your desired title here
+        });
+    }
+}
