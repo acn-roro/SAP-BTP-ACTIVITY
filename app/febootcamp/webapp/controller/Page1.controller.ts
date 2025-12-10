f5b753bb714040185b00b66b2e3422f1e0576ad4
Ronan Chavarria <ronan.chavarria@accenture.com>
2025-12-10 11:02:19 +0000

    does have error

--- app/febootcamp/webapp/controller/Page1.controller.ts
+++ app/febootcamp/webapp/controller/Page1.controller.ts
@@ -2,20 +2,61 @@
 import Controller from "sap/ui/core/mvc/Controller";
 import UIComponent from "sap/ui/core/UIComponent";
 import MessageBox from "sap/m/MessageBox";
+import JSONModel from "sap/ui/model/json/JSONModel";  
+import ODataModel from "sap/ui/model/odata/v2/ODataModel";
 
 export default class Page1 extends Controller {
 
-
-  public onButtonPress(): void {
-    MessageBox.show("Nice to meet you!", {
-      title: "Hello World",
-           icon: MessageBox.Icon.NONE,
-      actions: [MessageBox.Action.OK],
-    });
-  }
-    public onGoToPage2(): void {
-        const router = UIComponent.getRouterFor(this);
-       router.navTo("RoutePage2"); 
-
-    };
+   public onInit(): void{
+       
+        this.getView()?.setModel(new JSONModel());
+        const oModel = this.getOwnerComponent()?.getModel() as  ODataModel;
+        this.sUrl  = oModel.getServiceUrl();
+       
+        this.getBooks();
+    }
+ 
+    public onInit(): void{
+        const oModel = this.getOwnerComponent()?.getModel() as  ODataModel;
+        this.sUrl  = oModel.getServiceUrl();
+        console.log("hello");
+        this.getBooks();
+    }
+ 
+    public async getBooks() {
+        try {
+            console.log("hello");
+ 
+            const response = await fetch(this.sUrl + "Books", {
+                method: "GET",
+                headers: { "Content-Type": "application/json" }
+            });
+ 
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+ 
+            const data = await response.json();
+            console.log(data);
+ 
+            interface Books {
+                BorrowerName: string;
+                BookTitle: number;
+            }
+ 
+            const dataArray = data.value as Books[];
+ 
+            const mapped = dataArray.map(item => ({
+                borrowerName: item.BorrowerName,
+                bookTitle: item.UnitsInStock
+            }));
+ 
+            const oView = this.getView();
+            if (oView) {
+                oView.setModel(new JSONModel({ Products: mapped }));
+            }
+        } catch (error) {
+            console.error("Error fetching Products:", error);
+        }
+    }
   }
